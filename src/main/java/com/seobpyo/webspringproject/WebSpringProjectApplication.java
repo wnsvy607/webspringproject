5de0b048d9d6519333a3aed4d9e8f616f0f785af
wnsvy607 <wnsvy607@naver.com>
2022-08-30 00:29:15 +0900

    Initial commit

---
+++ src/main/java/com/seobpyo/webspringproject/WebSpringProjectApplication.java
@@ -0,0 +1,15 @@
+package com.seobpyo.webspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WebSpringProjectApplication.class, args);
+
+	}
+
+}
