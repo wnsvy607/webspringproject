613d60cdebfda8c3a7f078bba4835e375714c480
wnsvy607 <wnsvy607@naver.com>
2022-09-06 14:54:40 +0900

    mustache file addedq

--- src/main/java/com/seobpyo/webspringproject/web/IndexController.java
+++ src/main/java/com/seobpyo/webspringproject/web/IndexController.java
@@ -1,9 +1,10 @@
 package com.seobpyo.webspringproject.web;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
@@ -13,5 +14,10 @@ public class IndexController {
         return "index";
 //        앞으로는 src/main/resources/templates로, 뒤의 파일 확장자는 .mustache가 붙는다.
 //        즉, src/maun/resourcex/templates/index.mustache로 전환되어 View Resolver가 처리하게 됩니다.
+    }
+
+    @GetMapping("posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
