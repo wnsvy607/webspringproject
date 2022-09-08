6c8ddebfe656f5bf7dcfb3ed52ae3942b9e16f4a
wnsvy607 <wnsvy607@naver.com>
2022-09-08 12:36:29 +0900

    naver login added

--- src/main/java/com/seobpyo/webspringproject/web/IndexController.java
+++ src/main/java/com/seobpyo/webspringproject/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.seobpyo.webspringproject.web;
 
+import com.seobpyo.webspringproject.config.auth.LoginUser;
 import com.seobpyo.webspringproject.config.auth.dto.SessionUser;
 import com.seobpyo.webspringproject.service.posts.PostsService;
 import com.seobpyo.webspringproject.web.dto.PostsResponseDto;
@@ -17,13 +18,11 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
 
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName", user.getName());
